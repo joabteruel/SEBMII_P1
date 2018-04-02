f5ee990b40ae9b8e15de489ed791b7267441b5b7
Unknown <joab.gteruel@gmail.com>
2018-04-02 01:45:17 -0500

    Removed functions corresponding to UART control

--- usertask.h
+++ usertask.h
@@ -8,15 +8,6 @@
 #ifndef USERTASK_H_
 #define USERTASK_H_
 
-#include "fsl_uart.h"
-#include "fsl_clock.h"
-#include "pin_mux.h"
-#include "fsl_port.h"
-#include "MK64F12.h"
-
-uint8_t uart_configInit();
-
-
 
 
 
